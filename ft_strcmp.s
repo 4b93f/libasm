720123737b4c968abf7ec584997e5f4d3a1b0e56
chly-huc <chly-huc@student.s19.be>
2021-02-05 16:19:26 +0100

    ok

--- ft_strcmp.s
+++ ft_strcmp.s
@@ -2,11 +2,24 @@ global _ft_strcmp
 
 section .text
 
-_ft_strcmp:		mov rax, 0
-				jmp loop
+_ft_strcmp:	mov rax, 0
+			jmp loop
 
 _loop;
-				inc rax, 
-				inc 
-				inc rdi
-				mov cl, [rdi]
+			mov		al, BYTE [rdi] 
+			mov		bl, BYTE [rsi]	
+			cmp		al, 0		
+			je		exit		
+			cmp		bl, 0			
+			je		exit
+			cmp		al, bl			
+			jne		exit			
+			inc		rdi				
+			inc		rsi				
+			jmp		_ft_strcmp	
+
+_exit;
+			mov rax,  al
+			mov rbx, bl
+			sub rax, rbx
+			ret
---
+++ ft_strcpy.s
@@ -0,0 +1,6 @@
+				global _ft_strcpy
+
+				section .text
+_ft_strcpy:
+				mov cl, [rdi]
+				
