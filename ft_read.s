9287f288bffc2664ba94adf7ff1382c42efc590a
chly-huc <chly-huc@student.s19.be>
2021-02-08 17:28:58 +0100

    g

--- ft_read.s
+++ ft_read.s
@@ -1,6 +1,16 @@
-global _ft_read
+				global _ft_read
+				extern ___error
+				extern __errno_location
 
-section .text
 _ft_read:		mov rax, 0x2000003
 				syscall
+				jc _error
+				ret
+
+_error:
+				push rax
+				pop rdx
+				call ___errno_location
+				mov rdx, [rax]
+				mov rax, -1
 				ret
--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: chly-huc <chly-huc@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/12/11 16:01:08 by chly-huc          #+#    #+#             */
-/*   Updated: 2021/02/07 17:55:32 by chly-huc         ###   ########.fr       */
+/*   Updated: 2021/02/08 16:00:33 by chly-huc         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,6 +15,7 @@
 #include <sys/stat.h>
 #include <fcntl.h>
 #include <stdlib.h>
+#include <errno.h>
 
 int ft_write(int c , char *str, int e);
 int ft_read(unsigned int fd, const char *str, size_t e);
@@ -27,7 +28,12 @@ int main()
 {
 	char *s1 = "YOLo";
 	char *s2;
-	
+	int fd;
+	char *buf;
+
+	fd = open("ok", O_RDONLY);
+	printf("%d\n", ft_read(fd, buf, 5));
+	printf("%s\n", buf);
 	//printf("%d\n", ft_strcmp("oui", "OUI"));
 	//printf("{%s}\n", ft_strdup(s1));
 }
