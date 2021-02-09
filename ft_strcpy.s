bb81a9bdd18391f41150f606ec33c6160ac37cb7
4b93f <charli.huchard25@gmail.com>
2021-02-09 19:17:53 +0100

    ok

--- ft_strcpy.s
+++ ft_strcpy.s
@@ -1,18 +1,18 @@
-				global ft_strcpy
+			global ft_strcpy
 
 ft_strcpy:
-			mov		rax, 0
-			mov		rcx, 0
+			mov rax, 0
+			mov rcx, 0
 			jmp loop
 			
 loop:
-			cmp		BYTE[rsi + rcx], 0
-			je		exit
-			mov		dl, BYTE[rsi + rcx]
-			mov		BYTE[rdi + rcx], dl
+			cmp BYTE[rsi + rcx], 0
+			je exit
+			mov dl, BYTE[rsi + rcx]
+			mov BYTE[rdi + rcx], dl
 			inc rcx
-			jmp		loop
+			jmp loop
 exit:
-	mov		BYTE[rdi + rcx], 0
-	mov		rax, rdi
+			mov BYTE[rdi + rcx], 0
+			mov rax, rdi
 			ret
